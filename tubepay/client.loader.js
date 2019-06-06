a696895f3ad11c764a09c5fe3b460d4230373706
Markus Behm <markus@ipbc.io>
2019-06-06 16:00:23 +0100

    Pre-release test of module with new paywall

---
+++ tubepay/client.loader.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+(async () => {
+  const addScript = (to, src, attrs = {}) => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = src;
+      Object.keys(attrs).forEach(key => script.setAttribute(key, attrs[key]));
+      script.onload = resolve;
+      script.onerror = reject;
+      to.appendChild(script);
+    });
+  };
+
+  const shared = ['runtime', 'vendor', 'common', 'protobuf'];
+  const script = document.currentScript;
+  const parent = script.parentNode;
+  const attribs = {};
+  for (let i = 0; i < script.attributes.length; ++i) {
+    const attrib = script.attributes[i];
+    if (attrib.specified && attrib.name !== 'src') attribs[attrib.name] = attrib.value;
+  }
+  Object.keys(attribs).forEach(attrib => script.removeAttribute(attrib));
+
+  await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre/tubepay/'}${bundle}${'.bundle.js'}`)));
+  await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre/tubepay/'}${'client'}${'.bundle.js'}`, attribs);
+})();
