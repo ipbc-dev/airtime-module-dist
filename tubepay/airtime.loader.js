a26bcc20d71e82c5665758ab28e613c69dac6e99
Markus Behm <markus@ipbc.io>
2019-06-10 11:42:08 +0100

    New pre-release test v6

--- tubepay/airtime.loader.js
+++ tubepay/airtime.loader.js
@@ -23,6 +23,6 @@
   }
   Object.keys(attribs).forEach(attrib => script.removeAttribute(attrib));
 
-  await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre5/tubepay/'}${bundle}${'.bundle.js'}`)));
-  await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre5/tubepay/'}${'airtime'}${'.bundle.js'}`, attribs);
+  await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre6/tubepay/'}${bundle}${'.bundle.js'}`)));
+  await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre6/tubepay/'}${'airtime'}${'.bundle.js'}`, attribs);
 })();
