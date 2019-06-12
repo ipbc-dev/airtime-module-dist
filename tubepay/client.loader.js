8b5282da0a7b9051d5f6ced2e26063310580b943
Markus Behm <markus@ipbc.io>
2019-06-12 13:49:58 +0100

    New pre-release v8

--- tubepay/client.loader.js
+++ tubepay/client.loader.js
@@ -12,17 +12,22 @@
       to.appendChild(script);
     });
   };
+  try {
+    const shared = ['runtime', 'vendor', 'common', 'protobuf'];
+    const script = document.currentScript;
+    const parent = document.documentElement; // script.parentNode;
+    const attribs = {};
+    for (let i = 0; i < script.attributes.length; ++i) {
+      const attrib = script.attributes[i];
+      if (attrib.specified && attrib.name !== 'src') attribs[attrib.name] = attrib.value;
+    }
+    Object.keys(attribs).forEach(attrib => script.removeAttribute(attrib));
 
-  const shared = ['runtime', 'vendor', 'common', 'protobuf'];
-  const script = document.currentScript;
-  const parent = document.documentElement; // script.parentNode;
-  const attribs = {};
-  for (let i = 0; i < script.attributes.length; ++i) {
-    const attrib = script.attributes[i];
-    if (attrib.specified && attrib.name !== 'src') attribs[attrib.name] = attrib.value;
+    await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre8/tubepay/'}${bundle}${'.bundle.js'}`)));
+    await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre8/tubepay/'}${'client'}${'.bundle.js'}`, attribs);
+
+    document.dispatchEvent(new CustomEvent(`${'client'}-loaded`));
+  } catch (err) {
+    document.dispatchEvent(new CustomEvent(`${'client'}-load-error`, {detail: err.message || err}));
   }
-  Object.keys(attribs).forEach(attrib => script.removeAttribute(attrib));
-
-  await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre7/tubepay/'}${bundle}${'.bundle.js'}`)));
-  await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@0.9.9-pre7/tubepay/'}${'client'}${'.bundle.js'}`, attribs);
 })();
