84cc7aa3886d1734e76799bede30f32f95abd76c
Jorge Silva <j.silva@ipbc.io>
2020-08-12 11:39:57 +0100

    Version 1.0.2 tube4

--- tubepay/airtime.loader.js
+++ tubepay/airtime.loader.js
@@ -23,8 +23,8 @@
     }
     Object.keys(attribs).forEach(attrib => script.removeAttribute(attrib));
 
-    await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@1.0.1-tube4/tubepay/'}${bundle}${'.bundle.js'}`)));
-    await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@1.0.1-tube4/tubepay/'}${'airtime'}${'.bundle.js'}`, attribs);
+    await Promise.all(shared.map(bundle => addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@1.0.2-tube4/tubepay/'}${bundle}${'.bundle.js'}`)));
+    await addScript(parent, `${'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@1.0.2-tube4/tubepay/'}${'airtime'}${'.bundle.js'}`, attribs);
 
     document.dispatchEvent(new CustomEvent(`${'airtime'}-loaded`));
   } catch (err) {
